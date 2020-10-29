461fe1a33f9b1f2dd4990e750069a69a4cc5076d
unknown <nandhinimadan20@gmail.com>
2020-10-28 23:23:31 -0700

    copied middleware from guided project

--- users/userModel.js
+++ users/userModel.js
@@ -7,19 +7,28 @@ function getAllUsers(){
     return db("users").select("id","username")
 }
 //find by username
-async function getByUsername(data){
-              return await db("users")
+ function getByUsername(data){
+    console.log(data)
+              return  db("users")
                        .select("id","username","password")
                        .where(data)
 }
 async function insertUser(data){
-        const [id]= await db("users")
-                        insert(data)
+        const id= await db("users")
+                        .insert(data)
+        return getById(id)
+}
+async function getById(id){
+    return await db("users")
+                .select("id","username","password")
+                .where({id})
+                .first()
 }
 
 module.exports={
     getAllUsers,
     getByUsername,
     insertUser,
+    getById,
 
 }
--- users/userRouter.js
+++ users/userRouter.js
@@ -16,7 +16,8 @@ router.get("/users", async (req, res, next) => {
 router.post("/register", async (req, res, next) => {
     try {
         const { username, password } = req.body
-        const user = await users.getByUsername(username)
+        console.log(username)
+        const user = await users.getByUsername({ username }).first()
         if (user) {
             res.status(409).json({
                 message: "User already exists"
@@ -25,14 +26,62 @@ router.post("/register", async (req, res, next) => {
         else {
             const newuser = await users.insertUser({
                 username,
-                password: await bycrypt.harsh(password, 10) //password has been encrypted to 10 md5
+                password: await bycrypt.hash(password, 10) //password has been encrypted to 10 md5
             })
-            res.status(200).json({ newuser })
+            res.json({
+                newuser, data: "Succesfully inserted"
+            }
+            )
         }
     }
     catch (err) {
         next(err)
     }
+})
+
+//login
+router.post("/login",async(req,res,next)=>{
+    try {
+		const { username, password } = req.body
+		const user = await users.getByUsername({ username }).first()
+
+		if (!user) {
+			return res.status(401).json({
+				message: "Invalid Credentials",
+			})
+		}
+		//will be true or false depending on whether the password matched the hash
+		const passwordValid = await bycrypt.compare(password, user.password)
+		if (!passwordValid) {
+			return res.status(401).json({
+				message: "Invalid credentials",
+
+			})
+		}
+		res.json({
+			message: `Welcome ${user.username}!`,
+			data: `hai ${user.id}`
+		})
+	} catch (err) {
+		next(err)
+	}
+})
+router.get("/logout",async (req,res,next)=>{
+	try{
+		//delete the session on the server side, so the user is no longer authenticated
+		req.session.destroy((err)=>{
+			if(err){
+				next(err)
+
+			}
+			else{
+				res.status(204).end()
+			}
+		})
+	}
+	catch(err){
+		next(err)
+	}
 })
 
 module.exports = router
